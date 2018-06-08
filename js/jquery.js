058a3c06f71e006493a480f7e6195ae529bb2c76
nturpin <nova.t974@gmail.com>
2018-06-08 15:52:29 +0400

    correction

--- js/jquery.js
+++ js/jquery.js
@@ -1,10 +1,37 @@
-$(document).ready(function(){
-    $('.nav a').click(function(){
-        goToByScroll($(this).attr('href'));
-        return false;
+$(function() {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1000);
+            }
+        }
     });
 });
 
-function goToByScroll(id){
-    $('html,body').animate({scrollTop: $(id).offset().top},'slow');
-}
+$(function() {
+    /**
+    * Smooth scrolling to a specific element 
+    **/
+    function scrollTo( target ) {
+        if( target.length ) {
+            $("html, body").stop().animate( { scrollTop: target.offset().top }, 1000);
+        }
+    }
+    // exemple
+    scrollTo( $("#page-top") );
+});
+
+$(function() {
+    /**
+    * Smooth scrolling to the top of page
+    **/
+    $("html, body").animate({scrollTop : 0}, 1000);
+});
